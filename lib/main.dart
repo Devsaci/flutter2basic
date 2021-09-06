3c4b0fb95f8eb05a2065f085375fd988bc0afe69
saci zakaria <sacideveloppeur@gmail.com>
2021-09-06 16:17:56 +0200

    _MyAppState

--- lib/main.dart
+++ lib/main.dart
@@ -5,48 +5,43 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-   return MyAppState();
+    return _MyAppState();
   }
 }
- class MyAppState extends State<MyApp>{
 
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
 
-   int counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      counter += 2;
+    });
+  }
 
-   void incrementCounter() {
-     setState(() {
-       counter += 2;
-     });
-   }
-
-     @override
-     Widget build(BuildContext context) => MaterialApp(
-       home: Scaffold(
-         appBar: AppBar(
-           title: Text("AppBar Text"),
-         ),
-         body: Container(
-           alignment: Alignment.center,
-           color: Colors.amber,
-           child: Column(
-             mainAxisAlignment: MainAxisAlignment.center,
-             children: [
-               Text("Counter Textaddition"),
-               Text(
-                 '$counter',
-                 style: Theme.of(context).textTheme.headline1,
-               ),
-             ],
-           ),
-         ),
-         floatingActionButton:
-         FloatingActionButton(
-             onPressed: incrementCounter,
-           child: Icon(Icons.add_alarm),
-         ),
-
-       ),
-     );
-   }
-
-
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("AppBar Text"),
+          ),
+          body: Container(
+            alignment: Alignment.center,
+            color: Colors.amber,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Counter Textaddition"),
+                Text(
+                  '$counter',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: _incrementCounter,
+            child: Icon(Icons.add_alarm),
+          ),
+        ),
+      );
+}
