75bd19d01ebc043813dbd5dd371c9efa226dfdc9
saci zakaria <sacideveloppeur@gmail.com>
2021-09-06 17:01:26 +0200

    Text

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _MyAppState extends State<MyApp> {
                     str = "Hello";
                   });
                 },
-                child: ,
+                child: Text("Text RaisedBotton"),
                 ),
               ],
             )),
