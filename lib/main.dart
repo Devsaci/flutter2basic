d2445a4e5babe812150308a764f80fcc01b7f50d
saci zakaria <sacideveloppeur@gmail.com>
2021-09-06 16:05:02 +0200

    11. Stateless vs Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  int counter = 0;
-
-  void incrementCounter() {
-    counter += 2;
-  }
-
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("AppBar Text"),
-          ),
-          body: Container(
-            alignment: Alignment.center,
-            color: Colors.amber,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Counter Textaddition"),
-                Text(
-                  '$counter',
-                  style: Theme.of(context).textTheme.headline1,
-                ),
-              ],
-            ),
-          ),
-          floatingActionButton:
-              FloatingActionButton(onPressed: incrementCounter),
-        ),
-      );
+  State<StatefulWidget> createState() {
+   return MyAppState();
+  }
 }
+ class MyAppState extends State{
+
+
+   int counter = 0;
+
+   void incrementCounter() {
+     counter += 2;
+   }
+
+     @override
+     Widget build(BuildContext context) => MaterialApp(
+       home: Scaffold(
+         appBar: AppBar(
+           title: Text("AppBar Text"),
+         ),
+         body: Container(
+           alignment: Alignment.center,
+           color: Colors.amber,
+           child: Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               Text("Counter Textaddition"),
+               Text(
+                 '$counter',
+                 style: Theme.of(context).textTheme.headline1,
+               ),
+             ],
+           ),
+         ),
+         floatingActionButton:
+         FloatingActionButton(onPressed: incrementCounter),
+       ),
+     );
+   }
+
+
